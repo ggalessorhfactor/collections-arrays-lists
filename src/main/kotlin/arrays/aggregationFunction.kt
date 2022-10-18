852337fce13f24e132844b087bce2c7e05452c1d
Guilherme Galesso <guilherme.galesso@rhfactor.com.br>
2022-10-18 19:10:23 -0300

    Correction of the variable names

--- src/main/kotlin/arrays/aggregationFunction.kt
+++ src/main/kotlin/arrays/aggregationFunction.kt
@@ -1,16 +1,16 @@
 fun aggregationsFunctions() {
     val ages: IntArray = intArrayOf(12, 17, 18, 24, 35, 46, 64)
-    val olderAge = ages.max()
-    println("Older age: $olderAge")
+    val older = ages.max()
+    println("Older: $older")
 
-    val underage = ages.min()
-    println("Underage: $underage")
+    val newest = ages.min()
+    println("Newest: $newest ")
 
     val average: Double = ages.average()
     println("Average: $average")
 
-    val allBigger = ages.all { it >= 18}
-    println("All bigger? $allBigger")
+    val allAdults = ages.all { it >= 18}
+    println("All are adults? $allAdults")
 
     val someAdult = ages.any { it >= 18}
     println("Some adult? $someAdult")
